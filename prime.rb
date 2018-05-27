72f91f16487536ba48241e97dd0b6e3d8d53d399
Aziz Kosber <a.kosber@gmail.com>
2018-05-27 06:15:56 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def prime?(integer)
-  if integer < 0 || integer <= 1 || integer <= 3
+  if integer < 0 || integer <= 1
     false
   else
     (2..integer**0.5).to_a.none? {|i| integer % i == 0}
